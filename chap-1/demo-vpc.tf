588aaf08d71e80f8e3bd2dca2343ba5f9f75964b
Vibin Damodaran <vibin.damodaran@hmhco.com>
2021-12-17 13:26:28 -0500

    update the file with new support

--- chap-1/demo-vpc.tf
+++ chap-1/demo-vpc.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+  region     = "us-east-1"
 }
 
 ## Create VPC ##
 resource "aws_vpc" "terraform-vpc" {
-  cidr_block       = "172.16.0.0/16"
+  cidr_block           = "172.16.0.0/16"
   enable_dns_hostnames = true
-  tags {
+  tags = {
     Name = "terraform-demo-vpc"
   }
 }
 
 output "aws_vpc_id" {
-  value = "${aws_vpc.terraform-vpc.id}"
+  value = aws_vpc.terraform-vpc.id
 }
