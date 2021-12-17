cf15ffc21c0e674ddf1d366bfc06266ac81c9d5f
Vibin Damodaran <vibin.damodaran@hmhco.com>
2021-12-17 13:27:19 -0500

    update the file

--- chap-3/demo-subnets.tf
+++ chap-3/demo-subnets.tf
@@ -1,26 +1,26 @@
 provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+  region     = "us-east-1"
 }
 
 ## Create VPC ##
 resource "aws_vpc" "terraform-vpc" {
-  cidr_block       = "172.16.0.0/16"
+  cidr_block           = "172.16.0.0/16"
   enable_dns_hostnames = true
-  tags {
+  tags = {
     Name = "terraform-demo-vpc"
   }
 }
 
 output "aws_vpc_id" {
-  value = "${aws_vpc.terraform-vpc.id}"
+  value = aws_vpc.terraform-vpc.id
 }
 
 ## Security Group##
 resource "aws_security_group" "terraform_private_sg" {
   description = "Allow limited inbound external traffic"
-  vpc_id      = "${aws_vpc.terraform-vpc.id}"
+  vpc_id      = aws_vpc.terraform-vpc.id
   name        = "terraform_ec2_private_sg"
 
   ingress {
@@ -51,26 +51,26 @@ resource "aws_security_group" "terraform_private_sg" {
     to_port     = 0
   }
 
-  tags {
+  tags = {
     Name = "ec2-private-sg"
   }
 }
 
 output "aws_security_gr_id" {
-  value = "${aws_security_group.terraform_private_sg.id}"
+  value = aws_security_group.terraform_private_sg.id
 }
 
 ## Create Subnets ##
 resource "aws_subnet" "terraform-subnet_1" {
-  vpc_id     = "${aws_vpc.terraform-vpc.id}"
-  cidr_block = "172.16.10.0/24"
+  vpc_id            = aws_vpc.terraform-vpc.id
+  cidr_block        = "172.16.10.0/24"
   availability_zone = "us-east-1a"
 
-  tags {
+  tags = {
     Name = "terraform-subnet_1"
   }
 }
 
 output "aws_subnet_subnet_1" {
-  value = "${aws_subnet.terraform-subnet_1.id}"
+  value = aws_subnet.terraform-subnet_1.id
 }
